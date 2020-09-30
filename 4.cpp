e7288115da5fc35ab162906075ebab90c271d2f6
krishna-sharma-codes <72129994+krishna-sharma-codes@users.noreply.github.com>
2020-09-30 21:41:56 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -42,3 +42,49 @@ int main()
   
     return 0;  
 }  
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
+
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
+{ 
+	int i, j; 
+	for (i = 0; i < n-1; i++)	 
+	
+	for (j = 0; j < n-i-1; j++) 
+		if (arr[j] > arr[j+1]) 
+			swap(&arr[j], &arr[j+1]); 
+} 
+
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i = 0; i < size; i++) 
+		cout << arr[i] << " "; 
+	cout << endl; 
+} 
+
+int main() 
+{ 
+	int n;
+	cout<<"Enter Size:";
+	cin>>n;
+	int *arr = new int[n];
+	cout<<"Enter Elements:";
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	bubbleSort(arr, n); 
+	cout<<"Sorted array: \n"; 
+	printArray(arr, n); 
+	return 0; 
+} 
